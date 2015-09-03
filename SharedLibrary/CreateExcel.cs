0c0714dec28452cc2b857e260f3c1af171bd2687
jieaido <jieaido@163.com>
2015-09-03 23:17:24 +0800

    先就这吧.需要看一下quatz的参数传递方法

---
+++ SharedLibrary/CreateExcel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Quartz;
+
+namespace SharedLibrary
+{
+    public class CreateExcel:IJob
+    {
+        public void Execute(IJobExecutionContext context)
+        {
+            var path = Createpath(DateTime.Now.Year, DateTime.Now.Month);
+            if (!DirExist(DateTime.Now.Year,DateTime.Now.Month))
+            {
+                Directory.CreateDirectory(path);
+            }
+            
+        }
+
+        public bool DirExist(int year,int month)
+        {
+            return Directory.Exists(Createpath(year,month));
+        }
+
+        public string Createpath(int year, int month)
+        {
+            return  @"d:\报表\" + year + @"\" + month + @"\";
+        }
+    }
+}
