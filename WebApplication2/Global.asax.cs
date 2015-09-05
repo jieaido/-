a0da70b297b55c3079bc13bec49134ff1ad14d99
jieaido <jieaido@163.com>
2015-09-05 20:35:22 +0800

    先就这吧.

---
+++ WebApplication2/Global.asax.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.SessionState;
+using Quartz;
+using Quartz.Impl;
+using SharedLibrary;
+
+namespace WebApplication2
+{
+    public class Global : System.Web.HttpApplication
+    {
+
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            ISchedulerFactory schedFact = new StdSchedulerFactory();
+
+            // get a scheduler
+            IScheduler sched = schedFact.GetScheduler();
+            sched.Start();
+
+
+            IJobDetail job = JobBuilder.Create<CreateExcel>().WithIdentity("job1", "group1").Build();
+
+            ITrigger trigger =
+                TriggerBuilder.Create()
+                    .WithSchedule(CronScheduleBuilder.DailyAtHourAndMinute(20,34)).StartNow()
+                    .Build();
+
+            sched.ScheduleJob(job, trigger);
+            
+        }
+
+        protected void Session_Start(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_AuthenticateRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Session_End(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_End(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
